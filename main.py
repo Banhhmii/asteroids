9774a55a74342ae7fc8179d36fb3c4d75d11b41a
Banhhmii <tommyngo156@gmail.com>
2024-11-22 04:41:58 -0500

    added collision function to CircleShape class

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
+import sys
 from constants import *
 import pygame
+from player import *
+from asteroid import *
+from asteroidfield import * 
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updateable,drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+    
+    asteroidField = AsteroidField()
+    
+    x = SCREEN_HEIGHT / 2
+    y = SCREEN_WIDTH / 2
+    player = Player(x,y)
+    dt = 0
     
     while(True):
-        screen.fill((0,0,0))
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for obj in updateable:
+            obj.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision(player) == True:
+                print("Game over")
+                sys.exit()
+
+        screen.fill((0,0,0))
+
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
     
 
 if __name__ == "__main__":
